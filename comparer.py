71338240a0049eb6cae4acfc52a7487ea09ae1af
Denis <salvi.denis2007@gmail.com>
2026-04-08 19:07:08 +0300

    feat: add DataComparator for set operations

--- comparer.py
+++ comparer.py
@@ -18,3 +18,16 @@ class FileHandler:
         with open(filepath, 'w', encoding='utf-8') as f:
             for line in sorted(lines):
                 f.write(f"{line}\n")
+class DataComparator:
+    
+    def __init__(self, data1, data2):
+        self.data1 = set(data1)
+        self.data2 = set(data2)
+
+    def get_common(self):
+        """Повертає спільні рядки (перетин)."""
+        return self.data1.intersection(self.data2)
+
+    def get_different(self):
+        """Повертає рядки, що не повторюються (симетрична різниця)."""
+        return self.data1.symmetric_difference(self.data2)
